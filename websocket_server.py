c002cf8223028354b137508fc91a8e4b2417bf35
Fan Yang -Camel <fanyang1@ethz.com>
2023-12-11 20:51:04 +0100

    add path planning

--- websocket_server.py
+++ websocket_server.py
@@ -11,10 +11,11 @@ import ros_numpy
 from grid_map_msgs.msg import GridMap
 from sensor_msgs.msg import PointCloud2
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from shape_msgs.msg import Mesh
+from geometry_msgs.msg import PoseWithCovarianceStamped, PointStamped
+from nav_msgs.msg import Path
+# from shape_msgs.msg import Mesh
 from scipy.spatial.transform import Rotation
-from std_srvs.srv import Trigger, TriggerResponse
+from std_srvs.srv import Trigger, TriggerResponse, SetBool, SetBoolResponse
 
 import socket as socket_lib
 socket_lib.setdefaulttimeout(1e9)
@@ -26,7 +27,7 @@ data_test_ = {"data": None}
 
 data_ = {"odom": None, "point_cloud": None, "mesh": None, "path": None}
 
-commands_ = {"is_pointcloud": False, "is_mesh": False, "target_pose": None}
+commands_ = {"is_pointcloud": False, "is_mesh": False, "is_follow": False, "target_pose": None}
 
 init_data_ = {"tf_device_to_odom": None, "tf_odom_to_base": None}
 
@@ -34,24 +35,30 @@ class SocketRosNode:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('socket_ros_node', disable_signals=True)
+        
+        # fake TF to device
+        # set current odom
+        self.odom_to_base = np.eye(4)
+        init_data_["tf_device_to_odom"] = np.eye(4)
+        self.previous_command = True
 
         # Create subscribers
         self.point_cloud_sub = rospy.Subscriber('/point_cloud_filter_rsl/filter_and_merger_rsl', PointCloud2, self.point_cloud_callback)
         self.odometry_sub = rospy.Subscriber('/state_estimator/pose_in_odom', PoseWithCovarianceStamped, self.odometry_callback)
         self.mesh_sub = rospy.Subscriber('/elevation_mapping/elevation_map_raw', GridMap, self.mesh_callback)
+        self.plan_path_sub = rospy.Subscriber('/path', Path, self.plan_path_callback)
         
-        # set current odom
-        self.odom_to_base = np.eye(4)
+        # create a waypoint publisher
+        self.waypoint_pub = rospy.Publisher('/mp_waypoint', PointStamped, queue_size=10)
+    
         
         # Initialize a service server
         self.service = rospy.Service('generate_synthetic_data', Trigger, self.handle_generate_synthetic_data)
-        
+            
         # timer event callback to call the service
         # rospy.Timer(rospy.Duration(1.0), self.timer_callback)
         rospy.Timer(rospy.Duration(1.0), self.goal_callback) # 1hz
-        
-        # fake TF to device
-        init_data_["tf_device_to_odom"] = np.eye(4)
+        rospy.Timer(rospy.Duration(1.0), self.path_following_client) # 1hz
         
         # print statrting message
         rospy.loginfo("Socket ROS node started, starting data communicator... \n")
@@ -77,6 +84,53 @@ class SocketRosNode:
 
         return xyz_array
     
+    def path_following_client(self, event):
+        # if commands_["is_follow"] == self.previous_command:
+        #     return
+        # call the service to enable path following
+        rospy.wait_for_service('/enable_path_following')
+        try:
+            enable_call = rospy.ServiceProxy('/enable_path_following', SetBool)
+            resp = enable_call(commands_["is_follow"])
+            print(resp.message)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+    
+    def waypointPublisher(self, pose):
+        # publish the waypoint
+        waypoint_msg = PointStamped()
+        waypoint_msg.header.frame_id = "odom"
+        waypoint_msg.header.stamp = rospy.Time.now()
+        waypoint_msg.point.x = pose[0, 3]
+        waypoint_msg.point.y = pose[1, 3]
+        waypoint_msg.point.z = pose[2, 3] + 0.5
+        self.waypoint_pub.publish(waypoint_msg)
+        
+    def plan_path_callback(self, data):
+        # write the path to data_["path"], the path is at the base frame, need to convert it to device frame
+        # check if device to odom TF is received
+        if init_data_["tf_device_to_odom"] is None:
+            rospy.loginfo("No device to odom TF received yet, skipping path data")
+            return
+
+        if len(data.poses) < 2:
+            rospy.loginfo("No point in path received yet, skipping path data")
+            return
+        
+        # print("path received!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        
+        path = np.zeros((len(data.poses), 3))
+        for i in range(len(data.poses)):
+            path[i, 0] = data.poses[i].pose.position.x
+            path[i, 1] = data.poses[i].pose.position.y
+            path[i, 2] = data.poses[i].pose.position.z
+        # convert to device frame
+        device_to_base = init_data_["tf_device_to_odom"] @ self.odom_to_base
+        path_data = {"frame_pose": device_to_base.flatten().tolist(), "data": path.flatten().tolist()}
+        data_["path"] = path_data
+        # rospy.loginfo("Updated path data")
+        
+    
     def handle_generate_synthetic_data(self, request):
         self.generate_synthetic_data()
         # sysnthetic data generated successfully
@@ -87,20 +141,23 @@ class SocketRosNode:
         
     def goal_callback(self, event):
         if commands_["target_pose"] is not None:
-            # write the path to data_["path"]
-            device_to_base = init_data_["tf_device_to_odom"] @ self.odom_to_base
-            device_to_target = commands_["target_pose"]
-            device_to_base_pos = device_to_base[:3, 3]
-            device_to_target_pos = device_to_target[:3, 3]
-            # generate a path with 5 points
-            path = np.zeros((5, 3))
-            for i in range(5):
-                path[i, :] = device_to_base_pos + (device_to_target_pos - device_to_base_pos) * i / 4
-                # add some noise
-                path[i, :] += np.random.normal(0, 0.1, 3)
-            # convert to list of lists
-            path = path.flatten().tolist()
-            data_["path"] = path
+            # target pose is device to goal, and need odom_to_goal
+            odom_to_goal = np.linalg.inv(init_data_["tf_device_to_odom"]) @ commands_["target_pose"]
+            self.waypointPublisher(odom_to_goal)
+            # # write the path to data_["path"]
+            # device_to_base = init_data_["tf_device_to_odom"] @ self.odom_to_base
+            # device_to_target = commands_["target_pose"]
+            # device_to_base_pos = device_to_base[:3, 3]
+            # device_to_target_pos = device_to_target[:3, 3]
+            # # generate a path with 5 points
+            # path = np.zeros((5, 3))
+            # for i in range(5):
+            #     path[i, :] = device_to_base_pos + (device_to_target_pos - device_to_base_pos) * i / 4
+            #     # add some noise
+            #     path[i, :] += np.random.normal(0, 0.1, 3)
+            # # convert to list of lists
+            # path = path.flatten().tolist()
+            # data_["path"] = path
             commands_["target_pose"] = None
         
     def timer_callback(self, event):
@@ -145,7 +202,7 @@ class SocketRosNode:
     def point_cloud_callback(self, data):
         # Convert PointCloud2 to numpy array (N, 3) using ros_numpy
         pc_np = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(data)
-        print("size of the points: ", pc_np.shape[0])
+        # print("size of the points: ", pc_np.shape[0])
         pc_np = pc_np[::5, :]
         # round pc_np to 3 decimal places
         pc_np = np.round(pc_np, 2)
@@ -186,9 +243,10 @@ class SocketRosNode:
         transformation_matrix[:3, 3] = translation
         self.odom_to_base = transformation_matrix.copy()
         init_data_["tf_odom_to_base"] = transformation_matrix.copy()
-
+        # print("tf_device_to_odom: ", init_data_["tf_device_to_odom"])
+        
         transformation_matrix = init_data_["tf_device_to_odom"] @ self.odom_to_base
-        print("odom trans update info: ", translation)
+        # print("odom trans update info: ", translation)
         # transformation_matrix = init_data_["tf_device_to_odom"]
         transformation_matrix = transformation_matrix.flatten().tolist()
         odom_data = {"frame_pose": transformation_matrix}  # Convert to list of lists
@@ -252,6 +310,7 @@ def start_socket_server():
 @socket.on('target_pose')  # Listening for 'client_message' event
 def handle_client_message1(sid, message):
     commands_["target_pose"] = decode_transformation_matrix(message)
+    commands_["is_follow"] = False
     print(f"Received target message from client {sid}")
     print(commands_["target_pose"])
     
@@ -280,6 +339,13 @@ def handle_client_message4(sid, message):
     commands_["is_mesh"] = decode_bool(message)
     print(f"Received mesh command from client {sid}")
     print(commands_["is_mesh"])
+    
+@socket.on('go_to_target')  # Listening for 'client_message' event
+def handle_client_message5(sid, message):
+    # decode bool message
+    commands_["is_follow"] = decode_bool(message)
+    print(f"Received follow command from client {sid}")
+    print(commands_["is_follow"])
 
 def decode_transformation_matrix(json_string):
     # Parse the JSON string
@@ -323,13 +389,14 @@ def path_worker():
         if data_["path"] is not None:
             print("send path")
             socket.emit('path', json.dumps(data_["path"]))
+            # print(data_["path"])
             data_["path"] = None
         socket.sleep(0.5) # 5 Hz
         
 def odom_worker():
     while(1):
         if data_["odom"] is not None and init_data_["tf_device_to_odom"] is not None:
-            print("send odom to device")
+            # print("send odom to device")
             socket.emit('odom', json.dumps(data_["odom"]))
             data_["odom"] = None
         socket.sleep(0.1) # 10 Hz
@@ -342,6 +409,7 @@ def main():
             socket.start_background_task(pointcloud_worker)
             socket.start_background_task(mesh_worker)
             socket.start_background_task(odom_worker)
+            socket.start_background_task(path_worker)
             start_socket_server()
         except:
             pass
